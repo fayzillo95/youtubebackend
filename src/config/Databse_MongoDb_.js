f1f22677d4bce2852445fa9ee7fe780a2904b0a7
fayzillo95 <ovovovlululutvata@gmail.com>
2025-05-16 16:57:31 +0500

    cdscsa

--- src/config/Databse_MongoDb_.js
+++ src/config/Databse_MongoDb_.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export default async function mongoseDB_Connection(){
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(`${process.env.MONGODB_URI}` )
         console.log( process.env.MONGODB_URI,"\n Mongodb Atlasga ulandi ");
         return true;
     } catch (error) {
